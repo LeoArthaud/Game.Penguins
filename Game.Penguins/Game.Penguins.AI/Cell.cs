50e5ac32048e0940341b4083e6e5e16633c24c33
LouBege <wolfla@hotmail.fr>
2019-03-20 11:36:58 +0100

    Implementation function LoadMap

---
+++ Game.Penguins/Game.Penguins.AI/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Penguins.Core.Interfaces.Game.GameBoard;
+
+namespace Game.Penguins.AI
+{
+    public class Cell : ICell
+    {
+        public CellType CellType { get; }
+        public int FishCount { get; }
+        public IPenguin CurrentPenguin { get; }
+        public event EventHandler StateChanged;
+    }
+}
---
+++ Game.Penguins/Game.Penguins.AI/GameBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Penguins.Core.Interfaces.Game.GameBoard;
+
+namespace Game.Penguins.AI
+{
+    public class GameBoard : IBoard
+    {
+        public ICell[,] Board { get; }
+
+        public GameBoard()
+        {
+            Board = new ICell[8,8];
+        }
+    }
+}
