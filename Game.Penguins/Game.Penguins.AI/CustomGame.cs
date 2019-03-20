73ccc6d4ad4512fa1fa08e53fe5670757181076c
LouBege <wolfla@hotmail.fr>
2019-03-20 09:29:50 +0100

    Add summary on CustomGame

--- Game.Penguins/Game.Penguins.AI/CustomGame.cs
+++ Game.Penguins/Game.Penguins.AI/CustomGame.cs
@@ -7,38 +7,84 @@ using Game.Penguins.Core.Interfaces.Game.Players;
 
 namespace Game.Penguins.AI
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class CustomGame : IGame
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public IBoard Board { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public NextActionType NextAction { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public IPlayer CurrentPlayer { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public IList<IPlayer> Players { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public event EventHandler StateChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="playerName"></param>
+        /// <param name="playerType"></param>
         public void AddPlayer(string playerName, PlayerType playerType)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void StartGame()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public void PlacePinguinManual(int x, int y)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void PlacePinguin()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
         public void MoveManual(IMove action)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Move()
         {
             throw new NotImplementedException();
