9c3f2c541d2f626fc4f9a2ca40a15ad25d76f6bb
LouBege <wolfla@hotmail.fr>
2019-04-22 17:35:17 +0200

    Delete some useless using in class MoveOfHuman

--- Game.Penguins/Game.Penguins.Core/CustomGame/PlayerHuman/MoveOfHuman.cs
+++ Game.Penguins/Game.Penguins.Core/CustomGame/PlayerHuman/MoveOfHuman.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using Game.Penguins.Core.Interfaces.Game.GameBoard;
 
 namespace Game.Penguins.Core.CustomGame.PlayerHuman
