b2c98085ac66a14410b5aaba11fef40bc171a4a2
LouBege <wolfla@hotmail.fr>
2019-04-22 17:45:39 +0200

    Delete useless using in Coordinates

--- Game.Penguins/Game.Penguins.Core/CustomGame/Coordinates.cs
+++ Game.Penguins/Game.Penguins.Core/CustomGame/Coordinates.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Game.Penguins.Core.CustomGame
+﻿namespace Game.Penguins.Core.CustomGame
 {
     public class Coordinates
     {
