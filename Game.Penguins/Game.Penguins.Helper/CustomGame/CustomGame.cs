335937f82dbd88eeed30b796491db4259bc71185
DaceMoria <leo.arthaud@epsi.fr>
2019-06-05 09:01:49 +0200

    Refactor remove of penguin

--- Game.Penguins/Game.Penguins.Helper/CustomGame/CustomGame.cs
+++ Game.Penguins/Game.Penguins.Helper/CustomGame/CustomGame.cs
@@ -278,35 +278,11 @@ namespace Game.Penguins.Helper.CustomGame
                         PossibilitiesOfOrigin();
                         foreach (var possibility in PossibilitiesOrigin)
                         {
-                            Player playerCurrent = (Player)CurrentPlayer;
+                           
                             var list = move.CheckMove(possibility);
                             if (list.Count == 0)
                             {
-
-                                // Get cell
-                                Cell cell = (Cell)Board.Board[possibility.X, possibility.Y];
-
-                                // Add to the player number of point of the cell
-                                playerCurrent.Points += cell.FishCount;
-
-                                // Cell become water
-                                cell.CellType = CellType.Water;
-
-                                // Register points for log
-                                var cellPoints = cell.FishCount;
-
-                                // Cell have no fish
-                                cell.FishCount = 0;
-
-                                // Cell have no penguin
-                                cell.CurrentPenguin = null;
-
-                                // Apply change
-                                cell.ChangeState();
-
-                                // Log manual penguin movement
-                                ILog log = LogManager.GetLogger(GetType().ToString());
-                                log.Info($"{playerCurrent.Name} moved to cell ({possibility.X}, {possibility.Y}) and gained {cellPoints}.");
+                                SuppPenguin(possibility);
                             }
                         }
                         AffectedCurrentPlayer(ChangeType.Move);
@@ -422,6 +398,41 @@ namespace Game.Penguins.Helper.CustomGame
             }
         }
 
+        /// <summary>
+        /// Function for remove a penguin alone
+        /// </summary>
+        public void SuppPenguin(Coordinates possibility)
+        {
+            Player playerCurrent = (Player)CurrentPlayer;
+
+            // Get cell
+            Cell cell = (Cell)Board.Board[possibility.X, possibility.Y];
+
+            // Add to the player number of point of the cell
+            playerCurrent.Points += cell.FishCount;
+
+            // Cell become water
+            cell.CellType = CellType.Water;
+
+            // Register points for log
+            var cellPoints = cell.FishCount;
+
+            // Cell have no fish
+            cell.FishCount = 0;
+
+            // Cell have no penguin
+            cell.CurrentPenguin = null;
+
+            // Apply change
+            cell.ChangeState();
+
+            ILog log = LogManager.GetLogger(GetType().ToString());
+            log.Info($"{playerCurrent.Name} moved to cell ({possibility.X}, {possibility.Y}) and gained {cellPoints}.");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void EndGame()
         {
             List<bool> nbPenguin = new List<bool>();
